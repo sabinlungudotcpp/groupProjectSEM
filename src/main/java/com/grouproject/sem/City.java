97e8e59c682f7755112168c11f432c8e31c54675
Sabin Constantin Lungu (2B || !2B) <sabinlungu293@gmail.com>
2020-02-07 17:36:53 +0000

    Added code to get a specific City

---
+++ src/main/java/com/grouproject/sem/City.java
@@ -0,0 +1,10 @@
+package com.grouproject.sem;
+
+public class City {
+    public int cityId;
+    public String cityName;
+    public int countryCode;
+    public String district;
+    public int population;
+
+}
