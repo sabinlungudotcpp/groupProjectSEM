c11f1c8cb073517e5d17955642d134544fffc36e
Sabin Constantin Lungu (2B || !2B) <sabinlungu293@gmail.com>
2020-03-03 11:49:24 +0000

    Adding Unit Tests dependencies to pom.xml

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class MyTest {
+    @Test
+    void unitTest() {
+        assertEquals(5, 5);
+    }
+}
