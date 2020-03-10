796fb1f471b2d71c6739fccb30af2d2afdf3a2b6
Jonathan <40413556@live.napier.ac.uk>
2020-03-10 12:55:39 +0000

    travis yml updated 4

--- src/test/java/com/grouproject/sem/MyTest.java
+++ src/test/java/com/grouproject/sem/MyTest.java
@@ -3,6 +3,8 @@ package com.grouproject.sem;
 import com.grouproject.sem.App;
 import com.grouproject.sem.City;
 import com.grouproject.sem.Country;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -16,9 +18,14 @@ class MyTest {
      *   then create a container from said image with the run command
      *   "-e MYSQL_ROOT_PASSWORD=example -p 33060:3306" */
 
+    @BeforeAll
+    static void init() {
+        app.connect("localhost:33060");
+    }
+
     @Test
     void testAllCountriesOrderByPopulation() {
-        app.connect("localhost:33060");
+        //app.connect("localhost:33060");
 
         ArrayList<Country> temp = app.getAllCountriesOrderByPopulation();
         Country country = new Country("AZE", "Azerbaijan", "Asia", "Middle East", (float) 86600.00, 1991, 7734000, (float) 62.9, (float) 4127.00, (float) 4100.00, "Azärbaycan", "Federal Republic", "Heydär Äliyev", 144, "AZ");
@@ -29,12 +36,12 @@ class MyTest {
                 break;
             }
         }
-        app.disconnect();
+        //app.disconnect();
     }
 
     @Test
     void testTopPopulatedCity() { //Checks if the query returns the correct top populated country
-        app.connect("localhost:33060");
+        //app.connect("localhost:33060");
 
         ArrayList<City> temp = app.getTopNCitiesInTheWorld(3);
         City city = new City(1024,"Mumbai (Bombay)","IND","Maharashtra",10500000);
@@ -43,12 +50,12 @@ class MyTest {
             assertEquals(city.getId(), temp.get(0).getId());
         }
 
-        app.disconnect();
+        //app.disconnect();
     }
 
     @Test
     void testTopCapitalCities() {
-        app.connect("localhost:33060");
+        //app.connect("localhost:33060");
 
         ArrayList<City> tempCities = app.getTopNCapitalCitiesInWorld(3);
         City city = new City(2331,"Seoul","Korea","Seoul",9981619);
@@ -57,12 +64,12 @@ class MyTest {
              assertEquals(city.getId(), tempCities.get(0).getId());
          }
 
-         app.disconnect();
+         //app.disconnect();
     }
 
     @Test
     void testTopPopulatedCitiesLimit() { //Checks if the query returns the correct top populated country
-        app.connect("localhost:33060");
+        //app.connect("localhost:33060");
 
         ArrayList<City> temp = app.getTopNCitiesInTheWorld(3);
         City city = new City(1024,"Mumbai (Bombay)","IND","Maharashtra",10500000);
@@ -71,6 +78,11 @@ class MyTest {
             assertEquals (3, temp.size());
         }
 
+        //app.disconnect();
+    }
+
+    @AfterAll
+    static void finish() {
         app.disconnect();
     }
 
